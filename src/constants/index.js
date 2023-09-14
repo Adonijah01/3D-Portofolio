6ea20f558dd4e4fa17a6f513710e7e88744bb634
adonijah01 <adonijahsingoei@gmail.com>
2023-09-14 19:05:59 +0300

    portofolio updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -120,12 +120,27 @@ import {
       company_name: "Starbucks",
       icon: starbucks,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "March 2023 - June 2023",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
         "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+
+    {
+      title: "Code Coach",
+      company_name: "CodeJIKA",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "March 2023 - Now",
+      points: [
+      'Introducing Coding to young High School Students',
+      'Teaching them how to code in HTML, CSS, JavaScript and other related technologies',
+      'Helping them build their own projects',
+      'Helping them with their school projects',
+      
       ],
     },
     {
@@ -237,7 +252,7 @@ import {
         },
       ],
       image: jobit,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/adonijah01",
     },
     {
       name: "Trip Guide",
